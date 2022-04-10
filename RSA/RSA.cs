0f79d0c867b225e946998325b07d16d57d2381d5
raimax <4miner44@gmail.com>
2022-04-10 19:49:19 +0300

    commit

---
+++ RSA/RSA.cs
@@ -0,0 +1,15 @@
+﻿namespace RSA
+{
+    public static class Rsa
+    {
+        public static void Encrypt(string plainText, ulong p, ulong q)
+        {
+
+        }
+
+        public static void Decrypt(string encryptedText)
+        {
+
+        }
+    }
+}
