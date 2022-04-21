75334ea27fa52339a5cfa779702b5c2857a8b5dc
raimax <4miner44@gmail.com>
2022-04-22 00:56:33 +0300

    commit

--- RSA/MainWindow.xaml.cs
+++ RSA/MainWindow.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows;
+﻿using Microsoft.Win32;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
 
 namespace RSA
 {
@@ -7,9 +12,146 @@ namespace RSA
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly Rsa _rsa = new Rsa();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void BtnEncrypt_Click(object sender, RoutedEventArgs e)
+        {
+            ulong p = uint.Parse(Prime1.Text);
+            ulong q = uint.Parse(Prime2.Text);
+            ulong n = _rsa.FindN(p, q);
+            ulong phi = _rsa.FindPhi(p, q);
+            ulong exponent = _rsa.FindE(n, phi);
+
+            LabelN.Content = $"n: {n}";
+            LabelPhi.Content = $"phi: {phi}";
+            LabelE.Content = $"e: {exponent}";
+
+            byte[] plainTextBytes = Encoding.ASCII.GetBytes(EncryptionPlainText.Text);
+
+            _rsa.Encrypted = _rsa.Encrypt(plainTextBytes, exponent, n);
+
+            ulong d = _rsa.GeneratePrivateKey(exponent, phi);
+            LabelD.Content = $"d: {d}";
+
+            EncryptionCypherText.Text = ListToString(_rsa.Encrypted);
+        }
+
+        private string ListToString<T>(List<T> list)
+        {
+            string encryptedText = "";
+            foreach (T number in list)
+            {
+                encryptedText += number?.ToString() + " ";
+            }
+
+            return encryptedText.Trim();
+        }
+
+        private void BtnDecrypt_Click(object sender, RoutedEventArgs e)
+        {
+            int n = int.Parse(DecryptionN.Text);
+            Dictionary<string, int> primeNumbers = _rsa.FindPrimeNumbers(n);
+
+            ulong p = (ulong)primeNumbers["p"];
+            ulong q = (ulong)primeNumbers["q"];
+
+            ulong phi = _rsa.FindPhi(p, q);
+            ulong exponent = _rsa.FindE((ulong)n, phi);
+
+            ulong d = _rsa.GeneratePrivateKey(exponent, phi);
+
+            List<ulong> encryptedText = StringToList(DecryptionCypherText.Text);
+            byte[] decryptedText = _rsa.Decrypt(encryptedText, d, (ulong)n);
+
+            string decryptedTextString = string.Empty;
+
+            foreach (byte item in decryptedText)
+            {
+                decryptedTextString += item + " ";
+            }
+
+            DecryptionPlainText.Text = decryptedTextString.Trim();
+
+
+        }
+
+        private async void BtnSaveCypherText(object sender, RoutedEventArgs e)
+        {
+            if (_rsa.Encrypted.Count > 0)
+            {
+                await SaveFileToDisk("CypherText.txt", ListToString(_rsa.Encrypted));
+            }
+        }
+
+        private void BtnSavePublicKey(object sender, RoutedEventArgs e)
+        {
+            //
+        }
+
+        private List<ulong> StringToList(string s)
+        {
+            string[] stringArr = s.Split(" ");
+            List<ulong> list = new List<ulong>();
+
+            foreach (string str in stringArr)
+            {
+                list.Add(ulong.Parse(str));
+            }
+
+            return list;
+        }
+
+        private async void BtnSaveDecryptedText(object sender, RoutedEventArgs e)
+        {
+            if (_rsa.Decrypted is not null)
+            {
+                await SaveFileToDisk("DecryptedText.txt", Encoding.UTF8.GetString(_rsa.Decrypted));
+            }
+        }
+
+        private async Task SaveFileToDisk(string filePath, string content)
+        {
+            SaveFileDialog saveFileDialog = new();
+            saveFileDialog.FileName = filePath;
+            saveFileDialog.Filter = "Text Files | *.txt";
+            saveFileDialog.DefaultExt = "txt";
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                if (saveFileDialog.FileName != "")
+                {
+                    await File.WriteAllTextAsync(saveFileDialog.FileName, content);
+                }
+            }
+        }
+
+        private async Task OpenFileFromDisk()
+        {
+            OpenFileDialog openFileDialog = new();
+            openFileDialog.Filter = "Text Files | *.txt";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                if (openFileDialog.FileName != "")
+                {
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new(fileStream))
+                    {
+                        DecryptionCypherText.Text = await reader.ReadToEndAsync();
+                    }
+                }
+            }
+        }
+
+        private async void BtnLoadCypherText(object sender, RoutedEventArgs e)
+        {
+            await OpenFileFromDisk();
         }
     }
 }
--- RSA/RSA.cs
+++ RSA/RSA.cs
@@ -1,30 +1,48 @@
-﻿namespace RSA
+﻿using System;
+using System.Collections.Generic;
+
+namespace RSA
 {
     public class Rsa
     {
-        public void Encrypt(string plainText, ulong p, ulong q)
+        public List<ulong> Encrypted { get; set; } = new List<ulong>();
+        public byte[]? Decrypted { get; set; }
+
+        public List<ulong> Encrypt(byte[] plainText, ulong e, ulong n)
         {
-            ulong n = FindN(p, q);
-            ulong phi = FindPhi(p, q);
-            ulong e = FindE(n, phi);
+            List<ulong> encrypted = new();
+
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                encrypted.Add((ulong)(Math.Pow(plainText[i], e) % n));
+            }
+
+            return encrypted;
         }
 
-        public void Decrypt(string encryptedText)
+        public byte[] Decrypt(List<ulong> encryptedText, ulong d, ulong n)
         {
+            byte[] decrypted = new byte[encryptedText.Count];
 
+            for (int i = 0; i < encryptedText.Count; i++)
+            {
+                decrypted[i] = (byte)(Math.Pow(encryptedText[i], d) % n);
+            }
+
+            return decrypted;
         }
 
-        private ulong FindN(ulong p, ulong q)
+        public ulong FindN(ulong p, ulong q)
         {
             return p * q;
         }
 
-        private ulong FindPhi(ulong p, ulong q)
+        public ulong FindPhi(ulong p, ulong q)
         {
-            return p - 1 * q - 1;
+            return ((p - 1) * (q - 1));
         }
 
-        private ulong FindGcd(ulong a, ulong b)
+        public ulong FindGcd(ulong a, ulong b)
         {
             if (b == 0)
             {
@@ -34,15 +52,74 @@
             return FindGcd(b, a % b);
         }
 
-        private ulong FindE(ulong n, ulong phi)
+        public ulong FindE(ulong n, ulong phi)
         {
             ulong e = 2;
-            while (FindGcd(e, n) != 1 || FindGcd(e, phi) != 1)
+            while (FindGcd(e, phi) != 1)
             {
                 e++;
             }
 
             return e;
+        }
+
+        private bool IsPrime(ulong number)
+        {
+            if (number == 1) return false;
+            if (number == 2) return true;
+
+            var limit = Math.Ceiling(Math.Sqrt(number));
+
+            for (ulong i = 2; i <= limit; ++i)
+                if (number % i == 0)
+                    return false;
+            return true;
+
+        }
+
+        public ulong GeneratePrivateKey(ulong exponent, ulong phi)
+        {
+            ulong d = 1;
+
+            while (d * exponent % phi != 1)
+            {
+                d++;
+            }
+
+            return d;
+        }
+
+        public Dictionary<string, int> FindPrimeNumbers(int n)
+        {
+            int num = 33;
+            int i, j;
+            int count = 0;
+            List<int> nums = new List<int>();
+
+            for (i = 2; i < num; i++)
+            {
+                // check for divisibility
+                if (num % i == 0)
+                {
+                    count = 0;
+                    // check for prime number
+                    for (j = 1; j <= i; j++)
+                    {
+                        if (i % j == 0)
+                            count++;
+                    }
+                    if (count == 2)
+                    {
+                        nums.Add(i);
+                    }
+                }
+            }
+
+            Dictionary<string, int> result = new Dictionary<string, int>();
+            result.Add("p", nums[0]);
+            result.Add("q", nums[1]);
+
+            return result;
         }
     }
 }
